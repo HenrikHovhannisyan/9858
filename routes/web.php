d5f3bec9b6565ce7f1f286dddda16310a80b65af
HenrikHovhannisyan <henrik.hovhannisyan.91@gmail.com>
2025-04-23 16:36:41 +0400

    Fixed bugs

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\SendMailController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PagesController;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,14 +16,8 @@ use App\Http\Controllers\PagesController;
 |
 */
 
-Route::get('/', [PagesController::class, 'index'])->name('home');
-Route::get('/product', [PagesController::class, 'product'])->name('product');
-Route::get('/faq', [PagesController::class, 'faq'])->name('faq');
-Route::get('/terms-condition', [PagesController::class, 'termsCondition'])->name('terms-condition');
-Route::get('/privacy-policy', [PagesController::class, 'privacyPolicy'])->name('privacy-policy');
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::post('/send-mail', [SendMailController::class, 'sendMail'])->name('send.mail');
 
 Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
