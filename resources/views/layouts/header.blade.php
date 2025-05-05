a7183e0daee2b8cb6c3bbf8065c9be586127451f
HenrikHovhannisyan <henrik.hovhannisyan.91@gmail.com>
2025-05-05 13:33:52 +0400

    Fixed bug

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -13,6 +13,9 @@
                 <!-- Left Side Of Navbar -->
                 <ul class="navbar-nav m-auto">
                     <li class="nav-item">
+                        <a class="nav-link" href="{{ route('shop') }}">Shop</a>
+                    </li>
+                    <li class="nav-item">
                         <a class="nav-link" href="{{ route('luxury_delivery') }}">Luxury Delivery</a>
                     </li>
                     <li class="nav-item">
